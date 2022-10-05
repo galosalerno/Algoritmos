c72c69d50800c4e4ec243ba08f415de352ffcac1
galo salerno <galo@botmaker.com>
2022-10-05 19:11:32 -0300

    Leer archivo y guardar lineas en array

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,5 @@
+#include <sstream> 
+#include<conio.h>
 #include <iostream>
 #include <fstream>
 #include <locale.h> 
@@ -7,83 +9,109 @@
 #include <any>
 using namespace std;
 
-struct terminal{
-    string codigo;
-    string nombre; 
-    string ciudad;
-    string pais; 
-    string superficie;
-    string cantidad_terminales; 
-    string destinos_nacionales;
-    string destinos_internacionales;
-
-    void asignar_codigo(string value){
-        codigo = value;
-    }
-    void asignar_nombre(string value){
-        nombre = value;
-    }
-    void asignar_ciudad(string value){
-        ciudad = value;
-    }
-    void asignar_pais(string value){
-        pais = value;
-    }
-    void asignar_superficie(string value){
-        superficie = value;
-    }
-    void asignar_cant_terminales(string value){
-        cantidad_terminales = value;
-    }
-    void asignar_destinos_nacionales(string value){
-        destinos_nacionales = value;
-    }
-    void asignar_internacionales(string value){
-        destinos_internacionales = value;
-    }
-};
-
-void writeFile(string nombreArchivo){
-    ofstream archivo;
+// struct terminal{
+    // string codigo;
+    // string nombre; 
+    // string ciudad;
+    // string pais; 
+    // string superficie;
+    // string cantidad_terminales; 
+    // string destinos_nacionales;
+    // string destinos_internacionales;
+// 
+    // void asignar_codigo(string value){
+        // codigo = value;
+    // }
+    // void asignar_nombre(string value){
+        // nombre = value;
+    // }
+    // void asignar_ciudad(string value){
+        // ciudad = value;
+    // }
+    // void asignar_pais(string value){
+        // pais = value;
+    // }
+    // void asignar_superficie(string value){
+        // superficie = value;
+    // }
+    // void asignar_cant_terminales(string value){
+        // cantidad_terminales = value;
+    // }
+    // void asignar_destinos_nacionales(string value){
+        // destinos_nacionales = value;
+    // }
+    // void asignar_internacionales(string value){
+        // destinos_internacionales = value;
+    // }
+// };
+// 
+// void writeFile(string nombreArchivo){
+    // ofstream archivo;
     // Abrimos el archivo
-    archivo.open(nombreArchivo.c_str(), fstream::out);
+    // archivo.open(nombreArchivo.c_str(), fstream::out);
     // Y le escribimos redirigiendo
-    archivo << "dato a ingresar";
+    // archivo << "dato a ingresar";
     // Finalmente lo cerramos
-    archivo.close();
-}
+    // archivo.close();
+// }
+// 
+// 
+// void readFile(string nombreArchivo){
+    // string linea;
+    // ifstream archivoLecutra(nombreArchivo.c_str());
+   // Obtener línea de archivo, y almacenar contenido en "linea"
+    // terminal ter;
+    // while (getline(archivoLecutra, linea)) {
+        // vector<char> v(linea.begin(), linea.end());
+        // cout << "Linea : " << linea <<endl;
+        // string actual;
+        // for (int i = 0; i <= v.size(); i++) {
+            // if(v[i] == ' '){
+                // if(i == 0){
+                    // ter.codigo = actual;
+                // }else if(i == 1){
+                    // ter.nombre =  actual;
+                // }
+                // 
+                // actual = "";
+            // }
+            // actual = actual + v[i];
+        // }
+    // }
+    // cout << "Codigo " << ter.codigo <<endl;
+    // cout << "Nombre " << ter.nombre <<endl;
+// }
+// 
+int main () { 
+    string line; 
+    int count = 0; 
+    ifstream file("terminales.txt"); 
 
-
-void readFile(string nombreArchivo){
-    string linea;
-    ifstream archivoLecutra(nombreArchivo.c_str());
-    // Obtener línea de archivo, y almacenar contenido en "linea"
-    terminal ter;
-    while (getline(archivoLecutra, linea)) {
-        vector<char> v(linea.begin(), linea.end());
-        cout << "Linea : " << linea <<endl;
-        string actual;
-        for (int i = 0; i <= v.size(); i++) {
-            if(v[i] == ' '){
-                if(i == 0){
-                    ter.codigo = actual;
-                }else if(i == 1){
-                    ter.nombre =  actual;
-                }
-                
-                actual = "";
-            }
-            actual = actual + v[i];
-        }
+    while (getline(file, line)) {
+        count++; 
     }
-    cout << "Codigo " << ter.codigo <<endl;
-    cout << "Nombre " << ter.nombre <<endl;
+    
+    string arr[contador];
+
+    file.clear(); 
+    file.seekg(0, ios::beg); 
+
+    for (int i=0; i<count; i++) {
+
+        getline(file, arr[i]); 
+    }
+    file.close(); 
+
+    for (int i=0; i<count; i++) {
+
+        cout << arr[i]<<endl;
+    }
+
+    getch();
+    return 0;
 }
 
-int main() {
-    setlocale(LC_ALL, ""); //Usar acentos (?)
-    int nAtletas, mayor=0, posMayor=0;
 
-    string nombreArchivo = "terminales.txt";
-	readFile(nombreArchivo);
-}
+
+
+
